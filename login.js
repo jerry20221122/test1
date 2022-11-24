64054438833354578ae7b51f5831f1a8e09a53ef
root <123@qq.com>
2022-11-24 09:25:01 +0800

    第一次代码提交

---
+++ login.js
@@ -0,0 +1,19 @@
+function login(){
+//主线的代码
+//var i = 0;
+//var ii =1;
+//var iii = 2;
+
+	for(let i =0;i<10;i++){
+		
+	var i = 0;
+	var xx = 3;
+	var yy = 4;
+	var iii = 5;
+}
+
+	var i = 0;
+	var xx = 3;
+	var yy = 4;
+	var iii = 5;
+}
